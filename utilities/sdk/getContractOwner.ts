a6bf2da4e9931a1b7137085cc5ac9201bc558bb9
Fabian Ferno <fabianferno@gmail.com>
2024-03-04 20:37:38 +0530

    Remove owner[0]

--- utilities/sdk/getContractOwner.ts
+++ utilities/sdk/getContractOwner.ts
@@ -4,5 +4,5 @@ import { GAP } from "@show-karma/karma-gap-sdk";
 export async function getContractOwner(signer: SignerOrProvider): Promise<Hex> {
   const multicall = await GAP.getMulticall(signer);
   const owner = await multicall.owner?.();
-  return owner[0];
+  return owner;
 }
