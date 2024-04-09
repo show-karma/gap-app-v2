1b9c395c0857bac7b7483371c81ad32c5eaecfcf
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2024-04-09 15:28:59 -0300

    chore: removing tests console

--- pages/project/[projectId]/index.tsx
+++ pages/project/[projectId]/index.tsx
@@ -169,7 +169,6 @@ export const NestedLayout = ({ children }: Props) => {
 
   useEffect(() => {
     if (!project || !project?.chainID || !isAuth || !isConnected) {
-      console.log("aqui");
       setIsProjectOwner(false);
       setIsProjectOwnerLoading(false);
       return;
@@ -181,10 +180,8 @@ export const NestedLayout = ({ children }: Props) => {
         const walletClient = await getWalletClient({
           chainId: project.chainID,
         });
-        console.log("walletClient", walletClient);
         if (!walletClient) return;
         const walletSigner = await walletClientToSigner(walletClient);
-        console.log("walletSigner", walletSigner);
 
         await getProjectOwner(walletSigner || signer, project)
           .then((res) => {
