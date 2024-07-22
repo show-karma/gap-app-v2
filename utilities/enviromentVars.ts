85528ae96384f47e87f715649f1e4f3937e0904f
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2024-07-22 14:43:23 -0300

    fix: devUrl

--- utilities/enviromentVars.ts
+++ utilities/enviromentVars.ts
@@ -1,6 +1,5 @@
 const isDev = process.env.NEXT_PUBLIC_ENV === "staging";
-// const baseDevUrl = "https://gapstagapi.karmahq.xyz";
-const baseDevUrl = "https://0209-143-255-252-122.ngrok-free.app";
+const baseDevUrl = "https://gapstagapi.karmahq.xyz";
 
 export const envVars = {
   isDev,
