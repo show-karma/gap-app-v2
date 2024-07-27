26c41581d20684c892eb8042f92749a48d5ca78e
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-26 21:28:32 -0300

    feat: add badgeIcon & update CardReview with badgeIcon

--- components/Pages/Project/Review/CardReview.tsx
+++ components/Pages/Project/Review/CardReview.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-import { StarIcon } from "@/components/Icons";
+import { BadgeIcon, BadgeName } from "@/components/Icons/Badge";
 import { Button } from "@/components/Utilities/Button";
 import { DynamicStars } from "@/components/Utilities/DynamicStars";
 import React from "react";
 
 export const CardReview = () => {
   interface BadgeListProps {
-    icon: React.FC;
+    icon: React.JSX.Element;
     description: string;
     score: React.ReactNode;
   }
 
   const badgeList: BadgeListProps[] = [
     {
-      icon: StarIcon,
+      icon: <BadgeIcon badgeName={BadgeName.CLEAR_GOALS} />,
       description:
         "Clear Goals: Recognizes programs with well-defined goals. Every grant program has a goal, such as governance, impact, or education. Are these goals well explained so you can build a project aligned with them?",
       score: (
@@ -28,7 +28,7 @@ export const CardReview = () => {
       ),
     },
     {
-      icon: StarIcon,
+      icon: <BadgeIcon badgeName={BadgeName.SMOOTH_APPLICATION} />,
       description:
         "Smooth Application: Awards a seamless application process. Are they using a tech that facilitates the application process? Did they get back to you after the application, or was it just a poor form?",
       score: (
@@ -42,7 +42,7 @@ export const CardReview = () => {
       ),
     },
     {
-      icon: StarIcon,
+      icon: <BadgeIcon badgeName={BadgeName.FAIR_ROUNDS} />,
       description:
         "Fair rounds: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sem urna, sodales vel placerat sed, elementum a orci. Duis sit amet neque rutrum, suscipit enim tempus, dignissim erat. Etiam interdum dignissim pretium.",
       score: (
@@ -56,7 +56,7 @@ export const CardReview = () => {
       ),
     },
     {
-      icon: StarIcon,
+      icon: <BadgeIcon badgeName={BadgeName.EASY_TEACH} />,
       description:
         "Easy Tech: Awards programs with easily implementable technology. How hard is the tech? Are the docs easy to use or find?",
       score: (
@@ -70,7 +70,7 @@ export const CardReview = () => {
       ),
     },
     {
-      icon: StarIcon,
+      icon: <BadgeIcon badgeName={BadgeName.SUPPORTIVE_TEAM} />,
       description:
         "Supportive Team: Highlights programs with highly supportive teams. Whether technical or not, if you receive very helpful support after applying for a grant, issue this badge. – Post-Grant Support: Highlights strong post-grant support. How much do they help you after the application? Do they suggest related projects, possible connections, or interested people?",
       score: (
@@ -84,7 +84,7 @@ export const CardReview = () => {
       ),
     },
     {
-      icon: StarIcon,
+      icon: <BadgeIcon badgeName={BadgeName.GREAT_REVIEWERS} />,
       description:
         "Great Reviewers: Recognizes top-quality grant reviewers. They are impartial, select well-written projects, set clear goals, and explain the application process well.",
       score: (
@@ -98,7 +98,7 @@ export const CardReview = () => {
       ),
     },
     {
-      icon: StarIcon,
+      icon: <BadgeIcon badgeName={BadgeName.FAST_DISBURSEMENT} />,
       description:
         "Fast Disbursement: Commends quick fund disbursement processes. Did they complete the payment as soon as you completed the milestones? If yes, issue this badge.",
       score: (
@@ -119,9 +119,7 @@ export const CardReview = () => {
         {badgeList.map((badge, index) => (
           <div key={index} className="flex flex-col w-full">
             <div className="flex flex-row w-full items-center gap-2">
-              <div className="flex">
-                <badge.icon />
-              </div>
+              <div className="flex">{badge.icon}</div>
               <div className="flex">{badge.description}</div>
               <div className="flex">{badge.score}</div>
             </div>
