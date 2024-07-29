b84d85deb0d89dac04551c395d6b83a8b60bb671
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-29 19:49:37 -0300

    update: navbar-review with date in unix-timestamp sorted

--- components/Pages/Project/Review/CardReviewSummary.tsx
+++ components/Pages/Project/Review/CardReviewSummary.tsx
@@ -3,52 +3,75 @@ import { StarReviewIcon } from "@/components/Icons/StarReview";
 import { useState } from "react";
 import { CardReview } from "./CardReview";
 import { ChevronDown } from "@/components/Icons";
+import { formatDate } from "@/utilities/formatDate";
 
 export const CardReviewSummary = () => {
   const [isStarSelected, setIsStarSelected] = useState<number | null>(null); // ID
 
   interface MiniReviewSummaryProps {
     id: number;
-    date: string;
+    date: number; // UNIX Timestamp
     score: number;
   }
 
   const MiniReviewSummary: MiniReviewSummaryProps[] = [
     {
       id: 1,
-      date: "24 July, 2024",
+      date: 1727136000, // 24 July, 2024 in Unix timestamp
       score: 4.6,
     },
     {
       id: 2,
-      date: "25 July, 2024",
+      date: 1727222400, // 25 July, 2024 in Unix timestamp
       score: 4.4,
     },
     {
       id: 3,
-      date: "26 July, 2024",
+      date: 1727308800, // 26 July, 2024 in Unix timestamp
       score: 4.1,
     },
     {
       id: 4,
-      date: "27 July, 2024",
+      date: 1727395200, // 27 July, 2024 in Unix timestamp
+      score: 4.9,
+    },
+    {
+      id: 5,
+      date: 1727395200, // 27 July, 2024 in Unix timestamp
+      score: 4.9,
+    },
+    {
+      id: 6,
+      date: 1727395200, // 27 July, 2024 in Unix timestamp
+      score: 4.9,
+    },
+    {
+      id: 7,
+      date: 1727395200, // 27 July, 2024 in Unix timestamp
+      score: 4.9,
+    },
+    {
+      id: 8,
+      date: 1727395200, // 27 July, 2024 in Unix timestamp
       score: 4.9,
     },
   ];
 
   return (
     <div className="w-full flex flex-col">
-      <div className="w-full flex px-2 gap-2 ">
-        {MiniReviewSummary.map(
+      <div className="w-full flex px-2 gap-2 overflow-x-auto">
+        {MiniReviewSummary.sort((a, b) => b.date - a.date).map(
           (miniReview: MiniReviewSummaryProps, index: number) => (
             <div
               key={miniReview.id}
-              className={`w-fit flex flex-col justify-center items-center px-10 ${
+              className={`w-full flex flex-col justify-center sm:px-14 px-4 items-center text-center ${
                 MiniReviewSummary.length - 1 !== index &&
                 "border-r border-b-zinc-300"
               }`}
             >
-              <p>{miniReview.date}</p>
+              <p className="w-full">
+                {formatDate(new Date(miniReview.date * 1000))}
+              </p>
               <StarReviewIcon
                 props={{
                   className: `w-20 h-20 ${
@@ -58,7 +81,9 @@ export const CardReviewSummary = () => {
                 pathProps={{
                   className: "cursor-pointer",
                   onClick: () => {
-                    setIsStarSelected(index);
+                    setIsStarSelected((prev) =>
+                      prev === index ? null : index
+                    );
                   },
                 }}
               />
--- components/Pages/Project/Review/NavbarReview.tsx
+++ components/Pages/Project/Review/NavbarReview.tsx
@@ -4,8 +4,8 @@ import { CardReviewSummary } from "./CardReviewSummary";
 export const NavbarReview = () => {
   return (
     <div className="flex w-full flex-col justify-center">
-      <div className="w-full flex flex-row px-2 overflow-x-auto">
-        <div className="flex flex-row">
+      <div className="w-full flex flex-row px-2">
+        <div className="flex flex-row w-full">
           <CardReviewSummary />
         </div>
       </div>
