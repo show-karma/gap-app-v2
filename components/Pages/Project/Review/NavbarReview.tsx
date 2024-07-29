26c7bd0140a8b0a057c0812efcc7083377ac636b
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-29 16:38:29 -0300

    fix: remove cursor-pointer in NavbarReview

--- components/Pages/Project/Review/NavbarReview.tsx
+++ components/Pages/Project/Review/NavbarReview.tsx
@@ -5,7 +5,7 @@ export const NavbarReview = () => {
   return (
     <div className="flex w-full flex-col justify-center">
       <div className="w-full flex flex-row px-2 overflow-x-auto">
-        <div className="flex flex-row cursor-pointer">
+        <div className="flex flex-row">
           <CardReviewSummary />
         </div>
       </div>
