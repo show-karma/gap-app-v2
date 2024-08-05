fb3f70a9d2fa216d641ce7af17ed070c3fd311dc
Fabian Ferno <fabianferno@gmail.com>
2024-08-05 23:25:39 +0530

    Clean up UI and redirect

--- components/Dialogs/MergeProjectDialog.tsx
+++ components/Dialogs/MergeProjectDialog.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { FC, Fragment, ReactNode, useState, useEffect, } from "react";
+import { FC, Fragment, ReactNode, useState, useEffect } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Button } from "../Utilities/Button";
@@ -40,13 +40,14 @@ type MergeProjectProps = {
 };
 
 function SearchProject({
-
     setPrimaryProject,
 }: {
-
-    setPrimaryProject: (value: string) => void;
+    setPrimaryProject: (value: IProjectResponse) => void;
 }) {
-    const [results, setResults] = useState<ISearchResponse>({ communities: [], projects: [] });
+    const [results, setResults] = useState<ISearchResponse>({
+        communities: [],
+        projects: [],
+    });
     const [isSearchListOpen, setIsSearchListOpen] = useState<boolean>(false);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
@@ -69,22 +70,18 @@ function SearchProject({
         return setIsLoading(false);
     }, 500);
 
-
-    const renderItem = (
-        item: IProjectResponse,
-        href: string
-    ) => {
+    const renderItem = (item: IProjectResponse, href: string) => {
         return (
             <div
                 key={item.uid}
                 onClick={() => {
-                    setPrimaryProject(item.uid);
+                    setPrimaryProject(item);
                     closeSearchList();
                 }}
             >
                 <div className=":last:border-b-0 cursor-pointer select-none border-b border-slate-100 px-4 py-2 transition hover:bg-slate-200 dark:hover:bg-zinc-700">
                     <b className="max-w-full text-ellipsis font-bold text-black dark:text-zinc-100">
-                        {item?.details?.data.slug}
+                        {item?.details?.data.title}
                     </b>
                     <br />
                     <div className="text-gray-500 dark:text-gray-200">
@@ -107,7 +104,7 @@ function SearchProject({
 
     return (
         <div
-            className="relative flex flex-row items-center gap-3 rounded-lg h-max w-full bg-zinc-100 px-4 max-2xl:gap-1 max-2xl:px-2 text-gray-600 dark:text-gray-200 dark:bg-zinc-800"
+            className="relative mb-20 pb-20 flex flex-row items-center gap-3 rounded-lg h-max w-full bg-zinc-100 px-4 max-2xl:gap-1 max-2xl:px-2 text-gray-600 dark:text-gray-200 dark:bg-zinc-800"
             onBlur={() => closeSearchList()}
         >
             <MagnifyingGlassIcon className="h-5 w-5" />
@@ -116,7 +113,10 @@ function SearchProject({
                 placeholder="Search for the primary project"
                 className="w-full min-w-[160px] bg-transparent placeholder:text-gray-400 px-1 py-2 text-gray-600 dark:text-gray-200 border-none border-b-zinc-800 outline-none focus:ring-0"
                 onChange={(e) => debouncedSearch(e.target.value)}
-                onFocus={() => [...results.projects, ...results.communities].length > 0 && setIsSearchListOpen(true)}
+                onFocus={() =>
+                    [...results.projects, ...results.communities].length > 0 &&
+                    setIsSearchListOpen(true)
+                }
             />
             {isSearchListOpen && (
                 <div className="absolute left-0 top-10 mt-3 max-h-32 min-w-full overflow-y-scroll rounded-md bg-white dark:bg-zinc-800 py-4 border border-zinc-200">
@@ -133,19 +133,16 @@ function SearchProject({
                             <Spinner />
                         </div>
                     )}
-                    {!isLoading &&
-                        results.projects.length === 0 &&
-                        (
-                            <div className="flex flex-col items-center text-center">
-                                <div className="w-full text-center">No results found.</div>
-                            </div>
-                        )}
+                    {!isLoading && results.projects.length === 0 && (
+                        <div className="flex flex-col items-center text-center">
+                            <div className="w-full text-center">No results found.</div>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
     );
 }
-
 
 const pointerSchema = z.object({
     ogProjectUID: z.string().startsWith("0x").length(42),
@@ -161,9 +158,10 @@ export const MergeProjectDialog: FC<MergeProjectProps> = ({
             "flex items-center gap-x-1 rounded-md bg-primary-50 dark:bg-primary-900/50 px-3 py-2 text-sm font-semibold text-primary-600 dark:text-zinc-100  hover:bg-primary-100 dark:hover:bg-primary-900 border border-primary-200 dark:border-primary-900",
     },
 }) => {
-
     let [isOpen, setIsOpen] = useState(false);
-    const [primaryProject, setPrimaryProject] = useState<string>("");
+    const [primaryProject, setPrimaryProject] = useState<IProjectResponse | null>(
+        null
+    );
     const [isLoading, setIsLoading] = useState(false);
     const [validAddress, setValidAddress] = useState(true);
 
@@ -248,7 +246,7 @@ export const MergeProjectDialog: FC<MergeProjectProps> = ({
     };
 
     useEffect(() => {
-        if (primaryProject?.length) setValidAddress(isHexString(primaryProject));
+        if (primaryProject) setValidAddress(Boolean(primaryProject));
     }, [primaryProject]);
 
     return (
@@ -293,36 +291,48 @@ export const MergeProjectDialog: FC<MergeProjectProps> = ({
                                     >
                                         Merge current project with primary project
                                     </Dialog.Title>
-                                    <div className="flex flex-col gap-2 mt-8">
-                                        <label htmlFor="primaryProject">Primary Project UID</label>
-                                        <input
-                                            className="rounded border border-zinc-300  dark:bg-zinc-800 px-2 py-1 text-black dark:text-white"
-                                            type="text"
-                                            value={primaryProject}
-                                            onChange={(e) => setPrimaryProject(e.target.value)}
-                                        />
+                                    <div className="flex flex-col gap-2 mt-2">
                                         <p className="text-red-500 mb-2">
-                                            {!validAddress && primaryProject?.length
+                                            {project?.symlinks?.length
+                                                ? `The current project is already primary project. Cannot be merged with another project. Please delete any existing pointers to enable merging.`
+                                                : null}
+                                        </p>
+                                        {
+                                            project && project.symlinks.length == 0 ? <>
+                                                {primaryProject ? (
+                                                    <div>
+                                                        <p className="mb-2">Selected Primary Project:</p>
+                                                        <p className="font-bold text-2xl">{`${primaryProject?.details?.data.title}`}</p>
+                                                        <p className="text-md">{`/${primaryProject?.details?.data.slug}`}</p>
+                                                    </div>
+                                                ) : (
+                                                    <div>Select a primary project to merge with.</div>
+                                                )}</> :
+                                                <div>
+                                                    {JSON.stringify(project?.uid)}
+                                                </div>
+                                        }
+
+                                        <p className="text-red-500 mb-2">
+                                            {!validAddress && primaryProject
                                                 ? `Invalid address. Address should be a hexadecimal string with
                 exactly 42 characters.`
                                                 : null}
                                         </p>
-                                        <p className="text-red-500 mb-2">
-                                            {project?.symlinks?.length
-                                                ? `This project is primary project. Cannot be merged with another project. Please delete any existing pointers to enable merging.`
-                                                : null}
-                                        </p>
+
                                         <p className="text-zinc-500 mb-2">
-                                            {project?.pointers?.length
+                                            {project && project?.pointers?.length > 0
                                                 ? `This project has already been merged.
                                                 Are you sure you want to add another pointer to this project?`
                                                 : null}
+
+                                            {/* {JSON.stringify(project?.pointers)} */}
                                         </p>
                                     </div>
-                                    <SearchProject setPrimaryProject={
-                                        setPrimaryProject
-                                    } />
-                                    <div className="flex flex-row gap-4 mt-20 pt-20 justify-end">
+                                    {project?.symlinks?.length == 0 && (
+                                        <SearchProject setPrimaryProject={setPrimaryProject} />
+                                    )}
+                                    <div className="flex flex-row gap-4 justify-end">
                                         <Button
                                             className="text-zinc-900 text-lg bg-transparent border-black border dark:text-zinc-100 dark:border-zinc-100 hover:bg-zinc-900 hover:text-white disabled:hover:bg-transparent disabled:hover:text-zinc-900"
                                             onClick={closeModal}
@@ -330,21 +340,29 @@ export const MergeProjectDialog: FC<MergeProjectProps> = ({
                                         >
                                             Cancel
                                         </Button>
-                                        <Button
-                                            className="text-white text-lg bg-red-600 border-black  hover:bg-red-600 hover:text-white"
-                                            onClick={
-                                                async () => {
+                                        {project?.symlinks?.length == 0 && (
+                                            <Button
+                                                className="text-white text-lg bg-red-600 border-black  hover:bg-red-600 hover:text-white"
+                                                onClick={async () => {
                                                     if (!validAddress) return;
                                                     setIsLoading(true);
-                                                    await createProjectPointer({ ogProjectUID: primaryProject });
+                                                    if (primaryProject) {
+                                                        await createProjectPointer({
+                                                            ogProjectUID: primaryProject.uid,
+                                                        });
+                                                    }
+                                                }}
+                                                disabled={
+                                                    isLoading ||
+                                                    !primaryProject ||
+                                                    Boolean(project?.symlinks?.length)
                                                 }
-                                            }
-                                            disabled={isLoading || !primaryProject || Boolean(project?.symlinks?.length)}
-                                            isLoading={isLoading}
-                                            type="button"
-                                        >
-                                            Merge to primary project
-                                        </Button>
+                                                isLoading={isLoading}
+                                                type="button"
+                                            >
+                                                Merge to primary project
+                                            </Button>
+                                        )}
                                     </div>
                                 </Dialog.Panel>
                             </Transition.Child>
--- components/Dialogs/ProjectDialog/index.tsx
+++ components/Dialogs/ProjectDialog/index.tsx
@@ -452,7 +452,7 @@ export const ProjectDialog: FC<ProjectDialogProps> = ({
           // eslint-disable-next-line no-await-in-loop, no-promise-executor-return
           await new Promise((resolve) => setTimeout(resolve, 1500));
         }
-      });
+      })
 
       reset();
       setTeam([]);
