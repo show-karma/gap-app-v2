a0061e018dae22afb73b37e9892feb2e8228c871
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2024-08-09 17:33:09 -0300

    feat: hide request intro button

--- components/Pages/Project/ProjectPage/ProjectBlocks.tsx
+++ components/Pages/Project/ProjectPage/ProjectBlocks.tsx
@@ -37,13 +37,13 @@ export function ProjectBlocks() {
     //   link: "/",
     //   bg: "bg-[#ECE9FE]",
     // },
-    {
-      iconSrc: "/icons/intro.png",
-      title: "Request intro",
-      description: "Get an introduction to connect",
-      action: () => setIsIntroModalOpen(true),
-      bg: "bg-[#DBFFC5]",
-    },
+    // {
+    //   iconSrc: "/icons/intro.png",
+    //   title: "Request intro",
+    //   description: "Get an introduction to connect",
+    //   action: () => setIsIntroModalOpen(true),
+    //   bg: "bg-[#DBFFC5]",
+    // },
     {
       iconSrc: "/icons/endorsements.png",
       title: "Endorse the Project",
