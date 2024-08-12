f18f6275bc5fca3b81183bbd046cead3ec4cac73
heronlancellot <heronlancellot@id.uff.br>
2024-08-12 10:22:33 -0300

    update: use context data wit reviewStore zustand

--- components/Pages/Project/Review/CardNewReview.tsx
+++ components/Pages/Project/Review/CardNewReview.tsx
@@ -1,60 +1,15 @@
 "use client";
 
-import { BadgeIcon, BadgeName } from "@/components/Icons/Badge";
+import { BadgeIcon } from "@/components/Icons/Badge";
 import { Button } from "@/components/Utilities/Button";
-import React, { useState } from "react";
+import React from "react";
 import { DynamicStarsReview } from "./DynamicStarsReview";
 import { BadgeListProps, CardReviewMode } from "./CardReview";
+import { useReviewStore } from "@/store/review";
 
 export const CardNewReview = ({ id }: { id: number }) => {
-  const defaultInitialBadgeList: BadgeListProps[] = [
-    {
-      name: BadgeName.CLEAR_GOALS,
-      description:
-        "Clear Goals: Recognizes programs with well-defined goals. Every grant program has a goal, such as governance, impact, or education. Are these goals well explained so you can build a project aligned with them?",
-      score: 0,
-    },
-    {
-      name: BadgeName.SMOOTH_APPLICATION,
-      description:
-        "Smooth Application: Awards a seamless application process. Are they using a tech that facilitates the application process? Did they get back to you after the application, ou foi apenas um formulário genérico?",
-      score: 0,
-    },
-    {
-      name: BadgeName.FAIR_ROUNDS,
-      description:
-        "Fair rounds: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sem urna, sodales vel placerat sed, elementum a orci. Duis sit amet neque rutrum, suscipit enim tempus, dignissim erat. Etiam interdum dignissim pretium.",
-      score: 0,
-    },
-    {
-      name: BadgeName.EASY_TEACH,
-      description:
-        "Easy Tech: Awards programs with easily implementable technology. How hard is the tech? Are the docs easy to use or find?",
-      score: 0,
-    },
-    {
-      name: BadgeName.SUPPORTIVE_TEAM,
-      description:
-        "Supportive Team: Highlights programs with highly supportive teams. Whether technical or not, if you receive very helpful support after applying for a grant, issue this badge. – Post-Grant Support: Highlights strong post-grant support. How much do they help you after the application? Do they suggest related projects, possible connections, or interested people?",
-      score: 0,
-    },
-    {
-      name: BadgeName.GREAT_REVIEWERS,
-      description:
-        "Great Reviewers: Recognizes top-quality grant reviewers. They are impartial, select well-written projects, set clear goals, and explain the application process well.",
-      score: 0,
-    },
-    {
-      name: BadgeName.FAST_DISBURSEMENT,
-      description:
-        "Fast Disbursement: Commends quick fund disbursement processes. Did they complete the payment as soon as you completed the milestones? If yes, issue this badge.",
-      score: 0,
-    },
-  ];
-
-  const [newReview, setNewReview] = useState<BadgeListProps[]>(
-    defaultInitialBadgeList
-  );
+  const setNewReview = useReviewStore((state) => state.setNewReview);
+  const newReview = useReviewStore((state) => state.newReview);
 
   const handleSetRating = (index: number, rating: number) => {
     const updatedBadges = [...newReview];
