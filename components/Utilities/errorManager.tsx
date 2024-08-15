e78adf1382039978aaf65e9cc01c8c8842292225
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2024-08-15 18:20:43 -0300

    feat: handling rejected

--- components/Utilities/errorManager.tsx
+++ components/Utilities/errorManager.tsx
@@ -1,5 +1,14 @@
 import * as Sentry from "@sentry/nextjs";
 
 export const errorManager = (errorMessage: string, error: any) => {
+  if (error?.originalError?.code || error?.originalError?.message) {
+    const wasRejected =
+      error?.originalError?.code?.toLowerCase()?.includes("rejected") ||
+      error?.originalError?.message?.toLowerCase()?.includes("rejected");
+    if (wasRejected) {
+      return;
+    }
+  }
+
   Sentry.captureException(`${errorMessage}: ${error}`);
 };
