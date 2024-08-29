a132327a3fe6bdf21fb79513c9afdec679c806f7
0xneves <asguilift@hotmail.com>
2024-08-29 16:57:40 -0300

    fix: imoport order

--- utilities/review/getBadge.ts
+++ utilities/review/getBadge.ts
@@ -1,9 +1,11 @@
-import { readContract } from "viem/actions";
 import { createPublicClient, http } from "viem";
+import { readContract } from "viem/actions";
 import { arbitrum } from "viem/chains";
+
+import { Badge } from "@/types/review";
+
 import { BADGE_REGISTRY } from "./constants/constants";
 import { BADGE_REGISTRY_ABI } from "./constants/abi";
-import { Badge } from "@/types/review";
 
 const publicClient = createPublicClient({
   chain: arbitrum,
