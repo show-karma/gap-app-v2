ba318e883c42e3df75d6bd770fe215371e126b53
heronlancellot <heronlancellot@id.uff.br>
2024-08-29 10:19:19 -0300

    fix: remove unnused code

--- components/Pages/Project/Review/NavbarReview.tsx
+++ components/Pages/Project/Review/NavbarReview.tsx
@@ -13,7 +13,6 @@ import { useSearchParams } from "next/navigation";
 import { GrantStory } from "@/types/review";
 
 export const NavbarReview = () => {
-  const review = useReviewStore((state) => state.review);
   const isStarSelected = useReviewStore((state) => state.isStarSelected);
   const stories = useReviewStore((state) => state.stories);
   const setStories = useReviewStore((state) => state.setStories);
@@ -60,50 +59,51 @@ export const NavbarReview = () => {
   return (
     <div className="w-full flex flex-col">
       <div className="w-full flex px-2 gap-2 overflow-x-auto pb-4 relative scroller">
-        {stories
-          .sort((a, b) => b.timestamp - a.timestamp)
-          .map((storie: GrantStory, index: number) => (
-            <div
-              key={index}
-              className="flex flex-col justify-center items-center text-center relative"
-            >
-              <p className="w-full">
-                {formatDate(new Date(Number(storie.timestamp) * 1000))}
-              </p>
-              <div className="w-full flex flex-col items-center sm:px-14 px-4">
-                <StarReviewIcon
-                  props={{
-                    className: `w-20 h-20 ${
-                      isStarSelected === index && "text-[#004EEB]"
-                    }`,
-                  }}
-                  pathProps={{
-                    className: "cursor-pointer",
-                    fill: `${isStarSelected === index && "#004EEB"} `,
-                    onClick: () => {
-                      handleToggleReviewSelected(index);
-                    },
-                  }}
-                />
-                <p>
-                  {parseFloat(
-                    Number(storie.averageScore).toFixed(2).substring(0, 3)
-                  ) / 100}
+        {stories &&
+          stories
+            .sort((a, b) => b.timestamp - a.timestamp)
+            .map((storie: GrantStory, index: number) => (
+              <div
+                key={index}
+                className="flex flex-col justify-center items-center text-center relative"
+              >
+                <p className="w-full">
+                  {formatDate(new Date(Number(storie.timestamp) * 1000))}
                 </p>
-                {isStarSelected === index && (
-                  <div>
-                    <ChevronDown />
-                  </div>
-                )}
-                {index < review.length - 1 && (
-                  <div className="absolute right-0 top-1/2 h-3/4 w-[2px] bg-zinc-300 transform -translate-y-1/2"></div>
-                )}
+                <div className="w-full flex flex-col items-center sm:px-14 px-4">
+                  <StarReviewIcon
+                    props={{
+                      className: `w-20 h-20 ${
+                        isStarSelected === index && "text-[#004EEB]"
+                      }`,
+                    }}
+                    pathProps={{
+                      className: "cursor-pointer",
+                      fill: `${isStarSelected === index && "#004EEB"} `,
+                      onClick: () => {
+                        handleToggleReviewSelected(index);
+                      },
+                    }}
+                  />
+                  <p>
+                    {parseFloat(
+                      Number(storie.averageScore).toFixed(2).substring(0, 3)
+                    ) / 100}
+                  </p>
+                  {isStarSelected === index && (
+                    <div>
+                      <ChevronDown />
+                    </div>
+                  )}
+                  {index < stories.length - 1 && (
+                    <div className="absolute right-0 top-1/2 h-3/4 w-[2px] bg-zinc-300 transform -translate-y-1/2"></div>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
       </div>
       <div className="w-full flex flex-col">
-        {isStarSelected !== null && (
+        {isStarSelected !== null && stories && (
           <CardReview storie={stories[isStarSelected]} />
         )}
       </div>
