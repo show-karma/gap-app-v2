becadfb9df57973d852622c37da0c9d1f0b9668f
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2024-09-18 18:13:42 -0300

    feat: adding dynamic to project page

--- app/project/[projectId]/page.tsx
+++ app/project/[projectId]/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { ProjectOverviewLoading } from "@/components/Pages/Project/Loading/Overview";
-import ProjectPage from "@/components/Pages/Project/ProjectPage";
+import dynamic from "next/dynamic";
+
+const ProjectPage = dynamic(
+  () => import("@/components/Pages/Project/ProjectPage"),
+  {
+    loading: () => <ProjectOverviewLoading />,
+  }
+);
 
 const ProjectPageIndex = () => {
   return <ProjectPage />;
-  // return <ProjectOverviewLoading />;
 };
 
 export default ProjectPageIndex;
