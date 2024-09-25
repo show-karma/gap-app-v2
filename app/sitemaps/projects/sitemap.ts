265e4ee95787f018b63220758bfdee8dceb98c26
Fabian Ferno <fabianferno@gmail.com>
2024-09-25 15:55:17 +0530

    feat: Sitemap partitions for projects

--- app/sitemaps/projects/sitemap.ts
+++ app/sitemaps/projects/sitemap.ts
@@ -1,119 +1,71 @@
 import { chosenCommunities } from "@/utilities/chosenCommunities";
 import { MetadataRoute } from "next";
-
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const communities = chosenCommunities().map(
-    (community) => community.slug || community.uid
-  );
-  const fetchProjects = await Promise.all(
-    communities.map(async (communityID) => {
-      console.log("Fetching projects for community", communityID);
-      let raw: any[] = [];
-      let page = 0;
-      let totalPages = 1;
-      const itemsPerPage = 100;
-      while (page < totalPages) {
-        console.log(
-          "Fetching page",
-          page,
-          "of",
-          totalPages,
-          "for community",
-          communityID
-        );
-        const community = await fetch(
-          `https://gapapi.karmahq.xyz/communities/${communityID}/grants?pageLimit=${itemsPerPage}&page=${page}`
-        )
-          .then((res) => res.json())
-          .catch(() => {
-            console.log("Error fetching projects for community", communityID);
-            return undefined;
-          });
-        if (!community) return;
-        totalPages = Math.ceil(community?.pageInfo?.totalItems / itemsPerPage);
-        raw = raw.concat(community?.data);
-        page++;
-      }
-      if (!raw.length) return;
-
-      const grantsWithProjects = raw
-      if (!grantsWithProjects) return;
+import axios from "axios";
 
 
-      console.log("Grants with projects for community", communityID, grantsWithProjects.length);
 
-      return grantsWithProjects;
-    })
-  ).catch((error) => console.log(error));
+export async function generateSitemaps() {
+  // Fetch all communities
+  const response = await axios.get("https://gapapi.karmahq.xyz/communities")
+  const communities = response.data
+    .filter((community: any) => community?.details?.data?.slug !== undefined)
+    .map((_: any, index: number) => ({
+      id: index
+    }));
 
-  let final: MetadataRoute.Sitemap = [];
-  const uniqueProjectUrls = new Set<string>();
+  return communities as { id: number }[]
+}
+
+export default async function sitemap(
+  { id }: { id: number }
+): Promise<MetadataRoute.Sitemap> {
+  const response = await axios.get("https://gapapi.karmahq.xyz/communities")
+  const communities = response.data
+    .filter((community: any) => community?.details?.data?.slug !== undefined)
+    .map((_: any, i: number) => ({
+      id: i,
+      slug: _?.details?.data?.slug
+    }));
+
+  const community = communities[id].slug;
 
 
-  (fetchProjects as any[])
-    .flat()
-    .filter((grant) => grant?.project)
-    .forEach((grant) => {
-      const projectUrl = `https://gap.karmahq.xyz/project/${grant.project?.details?.data?.uid ||
-        grant.project?.details?.data?.slug ||
-        grant.project?.data?.slug ||
-        grant.project?.data?.uid ||
-        grant.project?.slug ||
-        grant.project?.uid}`;
+  if (community) {
+    console.log("Fetching sitemaps for community", community, id);
+    const response = await axios.get(`https://gapapi.karmahq.xyz/communities/${community}/sitemaps`);
+    const sitemaps = response.data;
 
-
-      // Add project URL only if it's not already in the set
-      if (!uniqueProjectUrls.has(projectUrl)) {
-        uniqueProjectUrls.add(projectUrl);
-        final.push({
-          url: projectUrl,
-          lastModified: new Date().toISOString(),
-          changeFrequency: "daily",
-          priority: 1,
-        });
-
-        // For impact page
-        final.push({
-          url: `https://gap.karmahq.xyz/project/${grant.project?.details?.data?.uid ||
-            grant.project?.details?.data?.slug ||
-            grant.project?.data?.slug ||
-            grant.project?.data?.uid ||
-            grant.project?.slug ||
-            grant.project?.uid}/impact`,
-          lastModified: new Date().toISOString(),
-          changeFrequency: "daily",
-          priority: 1,
-        });
-      }
-
-      // For grant pages
-      final.push({
-        url: `https://gap.karmahq.xyz/project/${grant.project?.details?.data?.uid ||
-          grant.project?.details?.data?.slug ||
-          grant.project?.data?.slug ||
-          grant.project?.data?.uid ||
-          grant.project?.slug ||
-          grant.project?.uid}/grants/${grant.uid}`,
+    let sitemap: MetadataRoute.Sitemap = [
+      ...sitemaps.projects.map((url: string) => ({
+        url,
         lastModified: new Date().toISOString(),
         changeFrequency: "daily",
         priority: 1,
-      });
-
-      // For milestone and updates pages
-      final.push({
-        url: `https://gap.karmahq.xyz/project/${grant.project?.details?.data?.uid ||
-          grant.project?.details?.data?.slug ||
-          grant.project?.data?.slug ||
-          grant.project?.data?.uid ||
-          grant.project?.slug ||
-          grant.project?.uid}/grants/${grant.uid}/milestones-and-updates`,
+      })),
+      ...sitemaps.impacts.map((url: string) => ({
+        url,
         lastModified: new Date().toISOString(),
         changeFrequency: "daily",
         priority: 1,
-      });
-    });
+      })),
+      ...sitemaps.grants.map((url: string) => ({
+        url,
+        lastModified: new Date().toISOString(),
+        changeFrequency: "daily",
+        priority: 1,
+      })),
+      ...sitemaps.milestonesAndUpdates.map((url: string) => ({
+        url,
+        lastModified: new Date().toISOString(),
+        changeFrequency: "daily",
+        priority: 1,
+      }))
+    ];
 
-  console.log("Totally ", final.length, " URLs generated on sitemap");
+    console.log("Totally ", sitemap.length, " URLs generated on sitemap");
 
-  return final;
+    return sitemap;
+  } else {
+    return [];
+  }
 }
--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  enabled: process.env.VERCEL_ENV === "production",
+  enabled: process.env.VERCEL_ENV === "production" && process.env?.NEXT_PUBLIC_DISABLE_SENTRY !== "true",
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
