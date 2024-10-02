3030d4f0abbb32f842e20e4e38c13cbd6683aa72
heronlancellot <heronlancellot@id.uff.br>
2024-10-02 16:49:02 -0300

    feat: add review-summary

---
+++ components/Pages/Project/Review/CardReviewSummary.tsx
@@ -0,0 +1,113 @@
+import { ReviewMode } from "@/types/review";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { Button } from "@/components/Utilities/Button";
+import { isAddressEqual } from "viem";
+import { DynamicStarsReview } from "./DynamicStarsReview";
+import { useProjectStore } from "@/store";
+import { useAccount, useSwitchChain } from "wagmi";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
+import toast from "react-hot-toast";
+import { arbitrum } from "viem/chains";
+import { useReviewStore } from "@/store/review";
+import { getBadgeIds } from "@/utilities/review/getBadgeIds";
+import { SCORER_ID } from "@/utilities/review/constants/constants";
+import { getBadge } from "@/utilities/review/getBadge";
+
+export const CardReviewSummary = () => {
+  const project = useProjectStore((state: any) => state.project);
+  const setIsOpenReview = useReviewStore((state: any) => state.setIsOpenReview);
+  const setActiveBadges = useReviewStore((state: any) => state.setActiveBadges);
+  const setActiveBadgeIds = useReviewStore((state: any) => state.setActiveBadgeIds);
+
+  const { openConnectModal } = useConnectModal();
+  const { switchChain } = useSwitchChain();
+  const { isConnected, address, chainId } = useAccount();
+
+  // Grab all recent badges and save on state
+  const handleStoryBadges = async () => {
+    const badgeIds = await getBadgeIds(SCORER_ID);
+    const badges = badgeIds && (await Promise.all(badgeIds.map((id) => getBadge(id))));
+    setActiveBadgeIds(badgeIds);
+    setActiveBadges(badges);
+  };
+
+  const handleReviewButton = () => {
+    if (!isConnected && openConnectModal) {
+      openConnectModal();
+    } else {
+      if (chainId != arbitrum.id) {
+        switchChain({ chainId: arbitrum.id });
+        toast.error("Must connect to Arbitrum to review");
+      } else {
+        setIsOpenReview(ReviewMode.WRITE);
+        handleStoryBadges();
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col w-full gap-5">
+      <div className="flex w-full justify-between items-center">
+        <div className="flex gap-2">
+          <DynamicStarsReview
+            totalStars={1}
+            rating={0}
+            setRating={() => {}}
+            mode={ReviewMode.READ}
+          />
+          <h2 className="text-base font-semibold font-['Open Sans'] leading-normal">
+            Reviews Summary
+          </h2>
+        </div>
+        {isConnected &&
+        project?.recipient &&
+        address &&
+        !isAddressEqual(project.recipient, address) ? ( // Check if the address is equal to the grant recipient address
+          <Button
+            disabled={false}
+            onClick={handleReviewButton}
+            className="bg-[#0E104D] gap-2 px-3 items-center"
+          >
+            <PlusIcon className="w-3.5 h-3.5" />
+            Add Review
+          </Button>
+        ) : (
+          !isConnected && (
+            <Button
+              disabled={false}
+              onClick={openConnectModal}
+              className="bg-[#0E104D] gap-2 px-3 items-center"
+            >
+              Connect Wallet
+            </Button>
+          )
+        )}
+      </div>
+      <div className="flex gap-8 border border-[#26252A] rounded-lg p-6">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-[#959FA8] text-xs leading-4 font-bold font-['Open Sans']">
+            Total Review
+          </h1>
+          <h2 className="font-medium text-[56px] leading-[56px] font-['Open Sans']">100</h2>
+          <p className="text-[#959fa8] text-sm font-normal font-['Open Sans'] leading-tight">
+            Typically reviewed 5 times per day
+          </p>
+        </div>
+        <div className="border border-[#26252A] h-full" />
+        <div>
+          <h1 className="text-[#959FA8] text-xs leading-4 font-bold font-['Open-Sans']">
+            Average Review
+          </h1>
+          <h2 className="font-medium text-[56px] leading-[56px] font-['Open Sans']">4.0</h2>
+          <DynamicStarsReview
+            totalStars={5}
+            rating={4}
+            setRating={() => {}}
+            mode={ReviewMode.READ}
+          />
+        </div>
+        <div>5 --------- 28%</div>
+      </div>
+    </div>
+  );
+};
--- components/Pages/Project/Review/index.tsx
+++ components/Pages/Project/Review/index.tsx
@@ -1,29 +1,14 @@
 "use client";
 /* eslint-disable @next/next/no-img-element */
-import toast from "react-hot-toast";
 import { useProjectStore } from "@/store";
 import { useReviewStore } from "@/store/review";
-
-import { isAddressEqual } from "viem";
-import { useAccount, useSwitchChain } from "wagmi";
-import { arbitrum } from "@wagmi/core/chains";
-import { useConnectModal } from "@rainbow-me/rainbowkit";
-
 import { IGrantResponse } from "@show-karma/karma-gap-sdk/core/class/karma-indexer/api/types";
-
 import { ReviewMode } from "@/types/review";
-import { StarIcon } from "@/components/Icons";
 import { Spinner } from "@/components/Utilities/Spinner";
-import { Button } from "@/components/Utilities/Button";
 import { NavbarReview } from "@/components/Pages/Project/Review/NavbarReview";
-
-import { SCORER_ID } from "@/utilities/review/constants/constants";
-import { getBadgeIds } from "@/utilities/review/getBadgeIds";
-import { getBadge } from "@/utilities/review/getBadge";
-
-import { XMarkIcon, ChevronLeftIcon } from "@heroicons/react/24/solid";
-
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
 import { CardNewReview } from "./CardNewReview";
+import { CardReviewSummary } from "./CardReviewSummary";
 
 interface GrantAllReviewsProps {
   grant: IGrantResponse | undefined;
@@ -36,43 +21,13 @@ export const ReviewSection = ({ grant }: GrantAllReviewsProps) => {
       <Spinner />
     </div>;
   }
-  const project = useProjectStore((state: any) => state.project);
   const isOpenReview = useReviewStore((state: any) => state.isOpenReview);
   const setIsOpenReview = useReviewStore((state: any) => state.setIsOpenReview);
-
-  const setActiveBadges = useReviewStore((state: any) => state.setActiveBadges);
-  const setActiveBadgeIds = useReviewStore((state: any) => state.setActiveBadgeIds);
-
-  const { openConnectModal } = useConnectModal();
-  const { isConnected, address, chainId } = useAccount();
-  const { switchChain } = useSwitchChain();
-
-  const handleReviewButton = () => {
-    if (!isConnected && openConnectModal) {
-      openConnectModal();
-    } else {
-      if (chainId != arbitrum.id) {
-        switchChain({ chainId: arbitrum.id });
-        toast.error("Must connect to Arbitrum to review");
-      } else {
-        setIsOpenReview(ReviewMode.WRITE);
-        handleStoryBadges();
-      }
-    }
-  };
-
-  // Grab all recent badges and save on state
-  const handleStoryBadges = async () => {
-    const badgeIds = await getBadgeIds(SCORER_ID);
-    const badges = badgeIds && (await Promise.all(badgeIds.map((id) => getBadge(id))));
-    setActiveBadgeIds(badgeIds);
-    setActiveBadges(badges);
-  };
 
   return (
     <div className="space-y-5 flex w-full flex-col items-start justify-start gap-8">
       <div className="flex w-full max-w-5xl flex-col gap-8">
-        <div className="flex w-full flex-col items-start justify-between gap-6 pb-8 border border-[#26252A] rounded-md">
+        <div className="flex w-full flex-col items-start justify-between gap-6 pb-8">
           {isOpenReview === ReviewMode.WRITE ? (
             <>
               <div className="flex w-full gap-2 p-4 bg-[#18171C] border border-[#18171C] items-center">
@@ -91,36 +46,7 @@ export const ReviewSection = ({ grant }: GrantAllReviewsProps) => {
           ) : (
             isOpenReview === ReviewMode.READ && (
               <>
-                <div className="flex flex-col w-full">
-                  <div className="flex w-full justify-between">
-                    <h2 className="text-2xl font-normal">
-                      All reviews of <b>{grant?.details?.data?.title}</b>
-                    </h2>
-                    {isConnected &&
-                    project?.recipient &&
-                    address &&
-                    !isAddressEqual(project.recipient, address) ? ( // Check if the address is equal to the grant recipient address
-                      <Button
-                        disabled={false}
-                        onClick={handleReviewButton}
-                        className="flex justify-center items-center gap-x-1 rounded-md bg-primary-50 dark:bg-primary-900/50 px-3 py-2 text-sm font-semibold text-primary-600 dark:text-zinc-100  hover:bg-primary-100 dark:hover:bg-primary-900 border border-primary-200 dark:border-primary-900"
-                      >
-                        <StarIcon />
-                        Review
-                      </Button>
-                    ) : (
-                      !isConnected && (
-                        <Button
-                          disabled={false}
-                          onClick={openConnectModal}
-                          className="flex justify-center items-center gap-x-1 rounded-md bg-primary-50 dark:bg-primary-900/50 px-3 py-2 text-sm font-semibold text-primary-600 dark:text-zinc-100  hover:bg-primary-100 dark:hover:bg-primary-900 border border-primary-200 dark:border-primary-900"
-                        >
-                          Connect Wallet
-                        </Button>
-                      )
-                    )}
-                  </div>
-                </div>
+                <CardReviewSummary />
                 <NavbarReview />
               </>
             )
