1358143f84cf4502630a1f8998b56d0123d78a7b
RafaDSan <rafaeldss12@gmail.com>
2024-10-15 14:49:12 -0300

    fix: fixed smart contracts updated

--- utilities/review/constants/index.ts
+++ utilities/review/constants/index.ts
@@ -6,7 +6,7 @@ export const ARB_ONE_SCHEMA_REGISTRY = "0xA310da9c5B885E7fb3fbA9D66E9Ba6Df512b78
 
 // The schema UID for EAS
 export const KARMA_EAS_SCHEMA_UID =
-  "0x7cf7c9d2dcff65ed9d3271c4b15efbe76a42bf36049f15ccf2b8153c89cc1366";
+  "0x215ea84cc93b044073f5240366c1ee273727d6a18242b5f1918dbebd9c6e8a08";
 // The Karma-Gap scorer ID constant
 export const SCORER_ID = 1;
 // The Karma-Gap scorer decimals amount
@@ -15,9 +15,9 @@ export const SCORER_DECIMALS = 18;
 // The Trustful-Karma contracts
 export const GRANT_REGISTRY = "0x2E6De0735b896dD248B08dcb9BA1f1f6Dd5bf1B7";
 export const BADGE_REGISTRY = "0x95d4123c5fA150B04dD56b0ab5141DEcB41725b0";
-export const TRUSTFUL_SCORER = "0xa402A493780AA7C40468336f6A5Afea5814891bF";
-export const RESOLVER_EAS = "0xbAc1B69837EB8d68294cBDF81acbfE57250264CF";
-export const RESOLVER_TRUSTFUL = "0x12Fe1E060B0B1c4Efc6d7A8dC9394ffC53842b78";
+export const TRUSTFUL_SCORER = "0x45921d3b90fBb9700F078d7887caa0e3D949C264";
+export const RESOLVER_EAS = "0xb054902Edf1cb8F8fBC4e442A222B936d981C6D3";
+export const RESOLVER_TRUSTFUL = "0xEE85D33c370d61321d2F932b0275F6C52E429C8e";
 
 export const RAILWAY_BACKEND = "https://trustful-karma-gap-backend-staging.up.railway.app";
 
