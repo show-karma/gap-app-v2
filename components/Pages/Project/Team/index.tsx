fdb3d950645fe02bd633b211603f023c191fff81
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2024-10-18 18:18:40 -0300

    fix: crash issue

--- components/Pages/Project/Team/index.tsx
+++ components/Pages/Project/Team/index.tsx
@@ -3,25 +3,32 @@
 import { useProjectStore } from "@/store";
 import { MemberCard } from "./MemberCard";
 import { ContributorProfileDialog } from "@/components/Dialogs/ContributorProfileDialog";
+import { InviteMemberDialog } from "@/components/Dialogs/Member/InviteMember";
 
 export const Team = () => {
   const { project } = useProjectStore((state) => state);
   //   check if it have some duplicated
 
-  const members = Array.from(
-    new Set([
-      project?.recipient,
-      ...(project?.members?.map((member) => member.recipient) || []),
-    ])
-  );
+  const members = project
+    ? Array.from(
+        new Set([
+          project?.recipient,
+          ...(project?.members?.map((member) => member.recipient) || []),
+        ])
+      )
+    : [];
+
   return (
     <div className="pt-5 pb-20 flex flex-col items-start gap-4">
       <ContributorProfileDialog />
-      <h3 className="font-semibold text-lg text-black dark:text-white">
-        Built by
-      </h3>
+      <div className="flex flex-row gap-2 w-full max-w-3xl justify-between">
+        <h3 className="font-semibold text-lg text-black dark:text-white">
+          Built by
+        </h3>
+        <InviteMemberDialog />
+      </div>
       <div className="flex flex-col gap-4 max-w-3xl w-full">
-        {members.length
+        {members?.length
           ? members?.map((member) => (
               <MemberCard key={member} member={member as string} />
             ))
