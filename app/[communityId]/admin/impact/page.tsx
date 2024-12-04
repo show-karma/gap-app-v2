185d9a4d3cfcd51e121630d95349ba5c4b5957a8
Fabian Ferno <fabianferno@gmail.com>
2024-12-05 02:32:31 +0530

    fix: Add debug info for prod test

--- app/[communityId]/admin/impact/page.tsx
+++ app/[communityId]/admin/impact/page.tsx
@@ -1,5 +1,18 @@
+"use client";
 import ImpactPage from "@/components/Pages/Admin/Impact";
+import { Suspense } from "react";
+import { Spinner } from "@/components/Utilities/Spinner";
 
 export default function Page() {
-  return <ImpactPage />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex w-full items-center justify-center">
+          <Spinner />
+        </div>
+      }
+    >
+      <ImpactPage />
+    </Suspense>
+  );
 }
