1ac404d2e3030deb0443b0a05b7142c89ceca980
Otavio Migliavacca Madalosso <otaviomadalosso@gmail.com>
2025-01-22 14:59:10 -0300

    enhance URL substitution of project uuid for slug

--- components/Pages/Project/ProjectWrapper.tsx
+++ components/Pages/Project/ProjectWrapper.tsx
@@ -312,7 +312,9 @@ export const ProjectWrapper = ({ projectId, project }: ProjectWrapperProps) => {
         ?.projectById(project.pointers[0].data?.ogProjectUID)
         .then((_project) => {
           if (_project) {
-            router.push(`/project/${_project?.details?.data?.slug}`);
+            const currentPath = window.location.pathname;
+            const newPath = currentPath.replace(`/project/${project.uid}`, `/project/${_project?.details?.data?.slug}`);
+            router.push(newPath);
           }
         });
     }
