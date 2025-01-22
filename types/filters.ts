d3232071a388daf704583b1598580efe627172bb
Fabian Ferno <fabianferno@gmail.com>
2025-01-22 18:23:25 +0530

    lint: Fix lint errors

--- types/filters.ts
+++ types/filters.ts
@@ -1,9 +1,6 @@
-import { Question } from "./questions";
-
 export type SortByOptions = "recent" | "completed" | "milestones";
 export type StatusOptions = "all" | "to-complete" | "completed" | "starting";
 export type Category = {
   id: number;
   name: string;
-  questions?: Question[];
 };
