d39e046e64207b6597c49d80de365930b366cfbf
Fabian Ferno <fabianferno@gmail.com>
2025-03-04 04:02:15 +0530

    fix: Resolve merge conflict in ProjectOptionsMenu z-index class

--- components/Pages/Project/ProjectOptionsMenu.tsx
+++ components/Pages/Project/ProjectOptionsMenu.tsx
@@ -138,11 +138,7 @@ export const ProjectOptionsMenu = () => {
       {isTransferOwnershipModalOpen ? (
         <TransferOwnershipDialog buttonElement={null} />
       ) : null}
-<<<<<<< dev
       <Menu as="div" className={`relative inline-block text-left z-10`}>
-=======
-      <Menu as="div" className="relative inline-block text-left z-100">
->>>>>>> main
         <div>
           <Menu.Button className="w-max bg-zinc-100 dark:bg-zinc-800 hover:bg-zinc-100 hover:dark:bg-zinc-800 text-black dark:text-white p-2 rounded-lg">
             <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
