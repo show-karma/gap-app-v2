56018bcb71e4933f557df42f2ce63dad8caec46c
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2025-05-20 21:14:16 -0300

    feat: removing updates page

--- app/project/[projectId]/updates/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { OutputsAndOutcomesLoading } from "@/components/Pages/Project/Loading/OutputsAndOutcomes";
-import { Spinner } from "@/components/Utilities/Spinner";
-
-export default function Loading() {
-  return <Spinner />;
-}
--- app/project/[projectId]/updates/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-import { PAGES } from "@/utilities/pages";
-
-export default function Page({ params }: { params: { projectId: string } }) {
-  // Redirect users from the updates page to the roadmap page
-  redirect(PAGES.PROJECT.ROADMAP.ROOT(params.projectId));
-}
