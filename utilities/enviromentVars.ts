da691f507c7f0ad3f844f0b207808bd25672f8ff
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2025-05-30 10:47:11 -0300

    feat: fix baseDevUrl var

--- utilities/enviromentVars.ts
+++ utilities/enviromentVars.ts
@@ -1,22 +1,19 @@
 const isDev = process.env.NEXT_PUBLIC_ENV === "staging";
-// const baseDevUrl = "https://gapstagapi.karmahq.xyz";
-const baseDevUrl = "http://localhost:3002";
 
 export const envVars = {
   isDev,
   NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
   NEXT_PUBLIC_KARMA_API: "https://api.karmahq.xyz/api",
-  NEXT_PUBLIC_GAP_INDEXER_URL: isDev
-    ? baseDevUrl
-    : "https://gapapi.karmahq.xyz",
+  NEXT_PUBLIC_GAP_INDEXER_URL: process.env
+    .NEXT_PUBLIC_GAP_INDEXER_URL as string,
   NEXT_PUBLIC_ALLO_V2_GRAPHQL_URL: isDev
     ? "https://indexer-staging.fly.dev/graphql"
     : "https://grants-stack-indexer-v2.gitcoin.co/graphql",
   NEXT_PUBLIC_IPFS_SPONSOR_URL: isDev
-    ? `${baseDevUrl}/ipfs`
+    ? `${process.env.NEXT_PUBLIC_GAP_INDEXER_URL}/ipfs`
     : "https://gapapi.karmahq.xyz/ipfs",
   NEXT_PUBLIC_SPONSOR_URL: isDev
-    ? `${baseDevUrl}/attestations/sponsored-txn`
+    ? `${process.env.NEXT_PUBLIC_GAP_INDEXER_URL}/attestations/sponsored-txn`
     : "https://gapapi.karmahq.xyz/attestations/sponsored-txn",
   RPC: {
     OPTIMISM: process.env.NEXT_PUBLIC_RPC_OPTIMISM as string,
