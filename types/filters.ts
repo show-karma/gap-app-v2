fd34d3fad956f9c0fcfda1ae20b55ad302770703
AngeloAyranji <angeloayranji@gmail.com>
2025-06-05 00:12:56 +0300

    feat: add sort by transaction count

--- types/filters.ts
+++ types/filters.ts
@@ -1,4 +1,4 @@
-export type SortByOptions = "recent" | "completed" | "milestones";
+export type SortByOptions = "recent" | "completed" | "milestones" | "transactions_asc" | "transactions_desc";
 export type StatusOptions = "all" | "to-complete" | "completed" | "starting";
 export type Category = {
   id: number;
