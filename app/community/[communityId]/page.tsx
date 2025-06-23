1a22e4a59271aaf3bd278459b98b094d327b2b99
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2025-06-23 16:56:31 -0300

    Remove bulk subscribe option since gitcoin indexer is shut down (#531) (#532)
    Co-authored-by: Mahesh Murthy <mahesh@mmurthy.com>

--- app/community/[communityId]/page.tsx
+++ app/community/[communityId]/page.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import { CommunityFeed } from "@/components/CommunityFeed";
 import { CommunityGrants } from "@/components/CommunityGrants";
-import { ReceiveProjectUpdates } from "@/components/Pages/ReceiveProjectUpdates";
 import type { SortByOptions, MaturityStageOptions } from "@/types";
 import {
   getCommunityData,
   getCommunityCategories,
 } from "@/utilities/queries/getCommunityData";
 import { pagesOnRoot } from "@/utilities/pagesOnRoot";
-import { communitiesToBulkSubscribe } from "@/utilities/subscribe";
 import type { ICommunityResponse } from "@show-karma/karma-gap-sdk/core/class/karma-indexer/api/types";
 
 type Props = {
@@ -36,18 +34,6 @@ export default async function Page(props: Props) {
   return (
     <div className="flex flex-row gap-6 w-full max-w-full sm:px-3 md:px-4 px-6 py-2">
       <div className="flex w-full max-w-full flex-col justify-start items-center gap-6">
-        <div className="w-full lg:hidden">
-          {communitiesToBulkSubscribe.includes(
-            (community as ICommunityResponse)?.details?.data?.slug as string
-          ) ? (
-            <ReceiveProjectUpdates
-              communityName={
-                (community as ICommunityResponse)?.details?.data?.name || ""
-              }
-            />
-          ) : null}
-        </div>
-
         <div className="flex gap-8 flex-row max-lg:flex-col-reverse w-full">
           <CommunityGrants
             categoriesOptions={categoriesOptions}
@@ -59,15 +45,6 @@ export default async function Page(props: Props) {
         </div>
       </div>
       <div className="flex flex-col gap-2 w-4/12 max-lg:w-full max-lg:hidden">
-        {communitiesToBulkSubscribe.includes(
-          (community as ICommunityResponse)?.details?.data?.slug as string
-        ) ? (
-          <ReceiveProjectUpdates
-            communityName={
-              (community as ICommunityResponse)?.details?.data?.name || ""
-            }
-          />
-        ) : null}
         <CommunityFeed />
       </div>
     </div>
