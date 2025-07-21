3af4006581e7ec9db51f036dcdaa1990d7b86236
Bruno De Masi <bruno@demasi.dev>
2025-07-21 19:06:42 -0300

    feat(project-impact): last X days beneath text

--- components/Pages/Project/ProjectPage/ProjectImpact.tsx
+++ components/Pages/Project/ProjectPage/ProjectImpact.tsx
@@ -24,7 +24,7 @@ export const ProjectImpact = ({ projectId, range = 30 }: ProjectImpactProps) => 
   return (
     <div className="flex flex-col gap-2">
       <p className="text-black dark:text-zinc-401 font-bold text-sm">
-        Project impact
+        Project Impact
       </p>
       <div className="flex flex-row  max-lg:flex-col gap-4">
         {metrics.transactions && metrics.transactions.value > 0 && (
@@ -34,9 +34,12 @@ export const ProjectImpact = ({ projectId, range = 30 }: ProjectImpactProps) => 
                 {formatCurrency(metrics.transactions.value)}
               </p>
               <div className="flex flex-row gap-3">
-                <p className="font-normal text-brand-gray text-sm dark:text-zinc-301">
-                  {pluralize("Transaction", metrics.transactions.value)} <span className="text-xs text-gray-500 dark:text-zinc-400">(last {range} days)</span>
-                </p>
+                <div className="flex flex-col">
+                  <p className="font-normal text-brand-gray text-sm dark:text-zinc-301">
+                    {pluralize("Transaction", metrics.transactions.value)}
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-zinc-400">(last {range} days)</p>
+                </div>
               </div>
             </div>
           </div>
@@ -48,9 +51,12 @@ export const ProjectImpact = ({ projectId, range = 30 }: ProjectImpactProps) => 
                 {formatCurrency(metrics.gitCommits.value)}
               </p>
               <div className="flex flex-row gap-3">
-                <p className="font-normal text-brand-gray text-sm dark:text-zinc-301">
-                  {pluralize("Git Commit", metrics.gitCommits.value)} <span className="text-xs text-gray-500 dark:text-zinc-400">(last {range} days)</span>
-                </p>
+                <div className="flex flex-col">
+                  <p className="font-normal text-brand-gray text-sm dark:text-zinc-301">
+                    {pluralize("Git Commit", metrics.gitCommits.value)}
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-zinc-400">(last {range} days)</p>
+                </div>
               </div>
             </div>
           </div>
@@ -62,9 +68,12 @@ export const ProjectImpact = ({ projectId, range = 30 }: ProjectImpactProps) => 
                 {formatCurrency(metrics.uniqueUsers.value)}
               </p>
               <div className="flex flex-row gap-3">
-                <p className="font-normal text-brand-gray text-sm dark:text-zinc-301">
-                  {pluralize("Unique Users Transactions", metrics.uniqueUsers.value)} <span className="text-xs text-gray-500 dark:text-zinc-400">(last {range} days)</span>
-                </p>
+                <div className="flex flex-col">
+                  <p className="font-normal text-brand-gray text-sm dark:text-zinc-301">
+                    {pluralize("Unique Users Transactions", metrics.uniqueUsers.value)}
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-zinc-400">(last {range} days)</p>
+                </div>
               </div>
             </div>
           </div>
