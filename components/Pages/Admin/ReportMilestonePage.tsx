c9068326cc848b3610c2481d1d04cf3797d8b129
Bruno De Masi <bruno@demasi.dev>
2025-10-24 11:54:00 -0300

    fix: Set not being converted to Boolean but probably object

--- components/Pages/Admin/ReportMilestonePage.tsx
+++ components/Pages/Admin/ReportMilestonePage.tsx
@@ -150,7 +150,7 @@ export const ReportMilestonePage = ({
     return allowedSet.size > 0 ? allowedSet : null;
   }, [isAdmin, isContractOwner, reviewerPrograms, communityId]);
 
-  const isAuthorized = isConnected && isAuth && (isAdmin || isContractOwner || (allowedProgramIds && allowedProgramIds.size > 0));
+  const isAuthorized = Boolean(isConnected && isAuth && (isAdmin || isContractOwner || (allowedProgramIds && allowedProgramIds.size > 0)));
 
   const [currentPage, setCurrentPage] = useState(1);
   const [sortBy, setSortBy] = useState("totalMilestones");
