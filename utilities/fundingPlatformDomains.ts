3ef4a7fdddd9b2f23e3baa7916f7a5836b468c72
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2025-10-29 12:35:04 -0300

    feat: add new funding platform domains for Filecoin, Polygon, and Scroll
    - Introduced development and production URLs for Filecoin and Polygon.
    - Added development and production URLs for Scroll, ensuring comprehensive support for new platforms.

--- utilities/fundingPlatformDomains.ts
+++ utilities/fundingPlatformDomains.ts
@@ -3,6 +3,18 @@ export const fundingPlatformDomains = {
     dev: "https://testapp.opgrants.io",
     prod: "https://app.opgrants.io",
   },
+  filecoin: {
+    dev: "https://app.filpgf.io",
+    prod: "https://app.filpgf.io",
+  },
+  polygon: {
+    dev: "https://founders.polygon.technology",
+    prod: "https://founders.polygon.technology",
+  },
+  scroll: {
+    dev: "https://grantsapp.scroll.io1",
+    prod: "https://grantsapp.scroll.io1",
+  },
   shared: {
     dev: "https://testapp.karmahq.xyz",
     prod: "https://app.karmahq.xyz",
