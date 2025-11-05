6b1f980daf85ab709afe4bee6fbb400f3664339d
Mahesh Murthy <mahesh@mmurthy.com>
2025-11-05 11:41:46 -0800

    Update meta tags

--- utilities/meta.ts
+++ utilities/meta.ts
@@ -1,27 +1,27 @@
 import { PROJECT_NAME } from "@/constants/brand";
 
 export const ogMeta = {
-  url: "https://karmahq.xyz/",
-  siteName: `${PROJECT_NAME} - Grantee Accountability Protocol`,
+  url: "https://www.karmahq.xyz/",
+  siteName: `${PROJECT_NAME} - Where builders get funded and ecosystems grow`,
   type: "website",
-  images: ["https://karmahq.xyz/assets/previews/homepage.png"],
+  images: ["https://www.karmahq.xyz/images/homepage/builder-hero.png"],
 };
 
 export const twitterMeta = {
   card: "summary_large_image",
-  title: `${PROJECT_NAME} - Grantee Accountability Protocol`,
+  title: `${PROJECT_NAME} - Where builders get funded and ecosystems grow`,
   description:
-    "GAP is a protocol to help community get visibility into grantee progress and grantees to build reputation.",
+    "Karma is a platform for builders and ecosystems. Builders showcase their work and build reputation. Ecosystems use our full stack solution to allocate funding and grow their ecosystems.",
   creator: "@karmahq_",
   creatorId: "1445787271513341963",
   site: "https://karmahq.xyz",
-  images: ["https://karmahq.xyz/assets/previews/homepage.png"],
+  images: ["https://www.karmahq.xyz/images/homepage/builder-hero.png"],
 };
 
 export const defaultMetadata = {
-  title: `${PROJECT_NAME} - Grantee Accountability Protocol`,
+  title: `${PROJECT_NAME} - Where builders get funded and ecosystems grow`,
   description:
-    "GAP is a protocol to help community get visibility into grantee progress and grantees to build reputation.",
+    "Karma is a platform for builders and ecosystems. Builders showcase their work and build reputation. Ecosystems use our full stack solution to allocate funding and grow their ecosystems.",
   icons: ["/favicon.ico"],
   openGraph: {
     ...ogMeta,
@@ -32,8 +32,8 @@ export const defaultMetadata = {
 };
 
 export const customMetadata = ({
-  title = `${PROJECT_NAME} - Grantee Accountability Protocol`,
-  description = "GAP is a protocol to help community get visibility into grantee progress and grantees to build reputation.",
+  title = `${PROJECT_NAME} - Where builders get funded and ecosystems grow`,
+  description = "Karma is a platform for builders and ecosystems. Builders showcase their work and build reputation. Ecosystems use our full stack solution to allocate funding and grow their ecosystems.",
 }) => {
   return {
     title,
