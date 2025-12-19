a71c0a938f99fe2c98c60a70c681e84494680f66
Bruno De Masi <bruno@demasi.dev>
2025-12-19 01:04:44 -0300

    refactor(app): extract address utility and consolidate donation types

--- types/donations.ts
+++ types/donations.ts
@@ -1,9 +1,6 @@
-export enum DonationType {
-  CRYPTO = "crypto",
-  FIAT = "fiat",
-}
-
 export enum PaymentMethod {
   CRYPTO = "crypto",
   FIAT = "fiat",
 }
+
+export { PaymentMethod as DonationType };
