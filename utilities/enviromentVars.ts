22bc4bd591bab0a43d1ca03ea7b39cfcc67b842d
Mahesh Murthy <mahesh@mmurthy.com>
2026-02-05 16:09:34 -0800

    remove gelato sponsored-txn

--- utilities/enviromentVars.ts
+++ utilities/enviromentVars.ts
@@ -9,9 +9,6 @@ export const envVars = {
   NEXT_PUBLIC_IPFS_SPONSOR_URL: isDev
     ? `${process.env.NEXT_PUBLIC_GAP_INDEXER_URL}/ipfs`
     : "https://gapapi.karmahq.xyz/ipfs",
-  NEXT_PUBLIC_SPONSOR_URL: isDev
-    ? `${process.env.NEXT_PUBLIC_GAP_INDEXER_URL}/attestations/sponsored-txn`
-    : "https://gapapi.karmahq.xyz/attestations/sponsored-txn",
   RPC: {
     MAINNET: process.env.NEXT_PUBLIC_RPC_MAINNET as string,
     OPTIMISM: process.env.NEXT_PUBLIC_RPC_OPTIMISM as string,
